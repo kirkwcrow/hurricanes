597a74ba048ea1b57749bc5ab8514e75c36ac233
Alex <kacloud@ncsu.edu>
2019-02-13 15:31:35 -0500

    resubmission

--- 2_present_results.py
+++ 2_present_results.py
@@ -8,11 +8,11 @@ wk_dir     = "D:\\System\\Documents\\ACADEMIC\\HF\\Data\\2018_12_21\\"
 file_perf  = '1_model_performance.csv'
 file_gars  = '1_garson_importance.csv'
 file_boot  = '1_bootstrap_results.csv'
-file_seq   = '1_seq_predictions_more_preds.csv'
+file_seq   = '1_seq_predictions_hwfi.csv'
 pred_names = 'pred_names.csv'
 
 ### MODEL PARAMETERS ###
-gar_preds   = ['vmax_hwrf','vmax_op_t0','V1'] #predictors to plot
+gar_preds   = ['vmax_hwfi','vmax_op_t0','V1'] # force plot these predictors
 
 pretty_models = {'nhc_mae':'NHC','val_mae':'Neural net','hwrf_mae':'HWRF'}
 
@@ -21,14 +21,14 @@ lead_times  = [3*(x+1) for x in range(16)]
 
 ### EXECUTE ###
 var_names = pd.read_csv(wk_dir+pred_names,index_col=0).to_dict()['Name']
-
-results = pd.read_csv(wk_dir+file_perf)
-ax1 = results.rename(columns=pretty_models).plot(x=['lead_time']
-            ,y=list(pretty_models.values())
-            ,figsize=(6.5,5)
-            ,ylim=0)
-ax1.set_ylabel('Mean absolute error (knots)')
-ax1.set_xlabel('Lead time (hours)')
+#
+#results = pd.read_csv(wk_dir+file_perf)
+#ax1 = results.rename(columns=pretty_models).plot(x=['lead_time']
+#            ,y=list(pretty_models.values())
+#            ,figsize=(6.5,5)
+#            ,ylim=0)
+#ax1.set_ylabel('Mean absolute error (knots)')
+#ax1.set_xlabel('Lead time (hours)')
 
 # GARSON VARIABLE IMPORTANCE
 gar=pd.read_csv(wk_dir+file_gars,index_col=0)
@@ -38,9 +38,10 @@ del gar['max']
 gar=gar.transpose()
 to_plot = gar.iloc[:,0:5].copy()
 to_plot.rename(columns = var_names,inplace=True)
-ax=to_plot.plot(ylim=0,figsize=(6.5,5))
+ax=to_plot.plot(ylim=(0,0.35),figsize=(6.5,5))
 ax.set_ylabel('Garson variable importance')
 ax.set_xlabel('Lead time (hours)')
+ax.legend(loc='left')
 
 gar_out = gar.transpose()
 
@@ -58,7 +59,7 @@ del orig['lead_time']
 seq=seq.merge(orig,on=['storm_id','date'],how='left',suffixes=('','_t0'))
 
 gil = seq[seq.lead_time == 24].copy()
-gil['true_ri'] = (gil.vmax - gil.vmax_t0 > threshold).astype(int)
+gil['true_ri'] = (gil.vmax - gil.vmax_t0 >= threshold).astype(int)
 model_skill = []
 models = ['nhc','hwrf','pred_seq','hwfi']
 for m in models:
@@ -75,13 +76,22 @@ for m in models:
     
     # compute score
     skill = (hits-hits_random)/(hits+miss+f_a-hits_random)
-    model_skill.append((hits,miss,f_a,skill))
+    model_skill.append((total,hits,miss,f_a,skill))
 
 gil_res = pd.DataFrame.from_records(model_skill,index=models
-                                    ,columns=['Hits',
+                                    ,columns=['Total',
+                                              'Hits',
                                               'Misses',
                                               'False alarms',
                                               'Gilbert skill score'])
+    
+gil_res.to_csv(wk_dir+'2017_oos_gilbert_skill.csv',index=True)
+
+# to Latex
+toprint= np.round(gil_res.loc[['nhc','hwfi','pred_seq'],:],3)
+toprint.index=['NHC','HWFI','NN']
+toprint.reset_index(inplace=True)
+print(toprint.to_latex(index=False).replace('\\toprule','\hline').replace('\\midrule','\hline').replace('\\bottomrule','\hline'))
 
 gil.to_csv(wk_dir+'2017_storm_preds_with_ri_detection.csv',index=False)
 
@@ -89,17 +99,19 @@ gil.to_csv(wk_dir+'2017_storm_preds_with_ri_detection.csv',index=False)
 # SEQUENTIAL TEST RESULTS
 seq = pd.read_csv(wk_dir+file_seq,index_col=0)
 
-def seq_results(df1,err_type='MAE',plot=True):
+def seq_results(df1,err_type='MAE',pred='pred_seq',plot=True):
     df = df1.copy()
-    for var in ['nhc','pred_seq','hwrf','hwfi']:
+    for var in ['nhc',pred,'hwrf','hwfi']:
         df[var+'_diff']=np.abs(df['vmax_'+var]-df.vmax)
         if err_type == 'MSE': df[var+'_diff']=df[var+'_diff']**2
 
     perf_leadtime = df.groupby('lead_time').agg('mean').iloc[:,-4:] ## CAREFUL HERE: indexing by position
     perf_leadtime.columns = ['NHC','Neural net','HWRF','HWFI']
     if plot: 
-        ax=perf_leadtime.plot(xlim=[df.lead_time.min()-1,df.lead_time.max()+1])
-            #'2017 out of sample performance by lead time ('+err_type+')')
+        ax=perf_leadtime.plot(xlim=[df.lead_time.min()-1,df.lead_time.max()+1],
+                                    color=('c','C1','limegreen','darkgreen'),
+                                    lw=1.8)
+            # https://matplotlib.org/users/dflt_style_changes.html
         y_lab = 'Mean absolute error (knots)'
         if err_type == 'MSE': y_lab = 'Mean squared error (knots)'
         ax.set_ylabel(y_lab)
@@ -113,12 +125,11 @@ def seq_res_basin(df,err_type='MAE'):
     fig, axes = plt.subplots(nrows=1, ncols=2)
     atl.plot(ax=axes[0],title='Atlantic',sharey=True,figsize=(9,5))
     pac.plot(ax=axes[1],title='Pacific',sharey=True)
-
     fig.suptitle('Model performance by basin ('+err_type+')')
 
 seq_results(seq,'MSE')
-seq_res_basin(seq)
-seq_res_basin(seq,'MSE')
+#seq_res_basin(seq)
+#seq_res_basin(seq,'MSE')
 
 # BOOTSTRAP SEQUENTIAL
 #seq = pd.read_csv(wk_dir+file_seq,index_col=0)
@@ -131,15 +142,17 @@ def bsample(df,by_id='none'):
         bootstr=groups.loc[np.random.randint(0,len(groups),size=len(groups))]
         return df.merge(bootstr,'right',by_id)
 
-for pred_var in ['pred_seq','hwrf']:
+for pred_var in ['pred_seq','hwfi']:
     seq['abs_err_'+pred_var] = np.abs(seq.vmax-seq['vmax_'+pred_var])
 
-def bootstrap_ci(df,k,quantiles):
+def bootstrap_ci(df,k,quantiles
+                 ,pred_err='abs_err_pred_seq'
+                 ,comp_err='abs_err_hwrf'):
     for i in range(k):
-        v_keep = ['abs_err_hwrf','abs_err_pred_seq','lead_time']
-        tmp=bsample(seq,'storm_id')[v_keep].groupby(
+        v_keep = ['lead_time',pred_err,comp_err]
+        tmp=bsample(df,'storm_id')[v_keep].groupby(
                 'lead_time').agg('mean')
-        tmp['diff'] = tmp['abs_err_pred_seq'] - tmp['abs_err_hwrf']
+        tmp['diff'] = tmp[pred_err] - tmp[comp_err]
         if i == 0: mae = tmp
         else: mae=mae.join(tmp['diff'],rsuffix=str(i))
     ci = mae.quantile(quantiles,axis=1).transpose()
@@ -147,8 +160,15 @@ def bootstrap_ci(df,k,quantiles):
     print(ci.head())
     return ci
 
-ci=bootstrap_ci(seq,100,[0.05,0.95])
+ci=bootstrap_ci(seq,100,[0.05,0.95],comp_err='abs_err_hwfi')
 a=seq_results(seq)
+plt.errorbar(ci.index,y=ci[0.05]+ci['margin'],yerr=ci.margin,fmt='none',elinewidth=1,ecolor='black',capsize=3)
+plt.axhline(y=0,linewidth=1, color='gray',ls='--')
+
+#%% Bootstrap CI for 
+cv = pd.read_csv(wk_dir+'1_model_preds.csv')
+ci = bootstrap_ci(cv,100,[0.05,0.95],pred_err='abs_err_pred',comp_err='abs_err_hwfi')
+a=seq_results(cv,pred='pred')
 plt.errorbar(ci.index,y=ci[0.05]+ci['margin'],yerr=ci.margin,fmt='none',elinewidth=1,ecolor='black',capsize=3)
 plt.axhline(y=0,linewidth=1, color='gray',ls='--')
 
@@ -174,30 +194,9 @@ for s_id in list(seq.index.unique()):
         ax.set_xlabel('Time since '+str(start_t.at[s_id,'date'])+' (hours)')
         plt.savefig(dv_dir+'Plots\\OOS_prediction_plots\\'+str(s_id)+'.png')
 
-#%%
-# BOOTSTRAP CROSS-VALIDATION
-
-results_out = results.copy()
-boot_raw = pd.read_csv(wk_dir+file_boot)
-n = len(boot_raw)
-alpha = 0.05
-mae_estimate = results['val_mae'] #np.mean(boot_raw)
-range = (np.percentile(boot_raw,100*(1-alpha/2),axis=0)
-        -np.percentile(boot_raw,100*alpha/2,axis=0))
-ci_lower     = mae_estimate-range/2
-ci_upper     = mae_estimate+range/2
-
-results['bootstr_mean'] = list(mae_estimate)
-results['bootstr_ci_lb'] = list(ci_lower)
-results['bootstr_ci_ub'] = list(ci_upper)
-
-results.plot(x=['lead_time']
-            ,y=['nhc_mae','hwrf_mae','bootstr_mean','bootstr_ci_lb','bootstr_ci_ub']
-            ,ylim=0
-            ,figsize=(8,5.5)
-            ,color=['C0','C1','k','C7','C7']
-            ,style=['-']*3+['--']*2
-            ,title='Model MAE by lead time\nwith bootstrapped NN performance, '+
-                str(100*(1-alpha))+'% CI (n=100 per lead time)')
-
-results_out['bootstrap_std'] = list(np.std(boot_raw))
+#%% 
+# Cross validation results table in LaTeX
+cv_res= np.round(pd.read_csv(wk_dir+file_perf),3)
+cv_res=cv_res.set_index('lead_time').reset_index()
+cv_res.columns = ['Lead time','N. Obs.','N. Storms','HWRF','HWFI','NHC','Neural net']
+print(cv_res.to_latex(index=False).replace('\\toprule','\hline').replace('\\midrule','\hline').replace('\\bottomrule','\hline'))
